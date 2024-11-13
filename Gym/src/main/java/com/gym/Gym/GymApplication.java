35891133c24b42d16f8fbc2235fde5819429b203
Juanito215 <juan.david.cuellar@correounivalle.edu.co>
2024-11-13 09:13:57 -0500

    Se arreglan errores del proyecto y se crea la clase usuario

--- Gym/src/main/java/com/gym/Gym/GymApplication.java
+++ Gym/src/main/java/com/gym/Gym/GymApplication.java
@@ -3,6 +3,7 @@ package com.gym.Gym;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GymApplication {
 
---
+++ Gym/src/main/java/com/gym/Gym/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.gym.Gym.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+
+    public String crearUsuario() {
+        return "Usuario creado";
+    }
+}
