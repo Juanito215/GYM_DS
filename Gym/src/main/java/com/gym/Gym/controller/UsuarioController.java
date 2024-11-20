860fd5df620a289df56476955b33287781ada77b
Juanito215 <juan.david.cuellar@correounivalle.edu.co>
2024-11-20 02:51:12 -0500

    El controlador, service y repository tienen el metodo de registrar y auntenticar el nuevo usuario

--- Gym/src/main/java/com/gym/Gym/controller/UsuarioController.java
+++ Gym/src/main/java/com/gym/Gym/controller/UsuarioController.java
@@ -15,19 +15,18 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    // Endpoint para registrar un nuevo usuario
+
     @PostMapping("/registro")
     public ResponseEntity<Usuario> registrarUsuario(@RequestBody Usuario usuario) {
         Usuario nuevoUsuario = usuarioService.registrarUsuario(usuario);
         return ResponseEntity.ok(nuevoUsuario);
     }
 
-    // Endpoint para iniciar sesión
+
     @PostMapping("/login")
     public ResponseEntity<String> loginUsuario(@RequestBody Usuario usuario) {
         Optional<Usuario> usuarioExistente = usuarioService.loginUsuario(usuario.getCorreo(), usuario.getContrasena());
         if (usuarioExistente.isPresent()) {
-            // Si el inicio de sesión es exitoso, imprimirá en consola desde el servicio
             return ResponseEntity.ok("Inicio de sesión exitoso");
         } else {
             return ResponseEntity.status(401).body("Credenciales incorrectas");
