83edfbe725d2cd56cbe680a4d48eb3d7ffea3f2e
Juanito215 <juan.david.cuellar@correounivalle.edu.co>
2024-11-22 16:51:05 -0500

    agrego la funcion de registrar ejercicio

--- Gym/src/main/java/com/gym/Gym/controller/EjerciciosController.java
+++ Gym/src/main/java/com/gym/Gym/controller/EjerciciosController.java
@@ -1,8 +1,21 @@
 package com.gym.Gym.controller;
 
+import com.gym.Gym.model.Ejercicios;
+import com.gym.Gym.service.EjercicioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/ejercicios")
 public class EjerciciosController {
 
-    public void nuevoEjercicio() {
+    @Autowired
+    private EjercicioService ejercicioService;
 
+    @PostMapping("/agregar")
+    public ResponseEntity<Ejercicios> agregarEjercicio(@RequestBody Ejercicios ejercicio) {
+        Ejercicios nuevoEjercicio = ejercicioService.registrarEjercicio(ejercicio);
+        return ResponseEntity.ok(nuevoEjercicio);
     }
 }
